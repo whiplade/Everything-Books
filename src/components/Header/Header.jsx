f9ee865f8144dba775ff91cd32da8d824df52278
whiplade <nmawira13@gmail.com>
2023-11-24 09:18:52 +0300

    updated header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from "../Navbar/Navbar";
 import SearchForm from "../SearchForm/SearchForm";
 import "./Header.css";
-// import { Link } from 'react-router-dom'; 
 
 const Header = () => {
   return (
@@ -11,7 +10,7 @@ const Header = () => {
             <Navbar />
             <div className='header-content flex flex-c text-center text-white'>
                 <h2 className='header-title text-capitalize'>find your book of choice.</h2><br />
-                <p className='header-text fs-18 fw-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae sapiente quibusdam consequatur perspiciatis facere laboriosam non nesciunt at id repudiandae, modi iste? Eligendi, rerum!</p>
+                <p className='header-text fs-18 fw-3'>All the books you need in one place!</p>
                 <SearchForm />
             </div>
         </header>
